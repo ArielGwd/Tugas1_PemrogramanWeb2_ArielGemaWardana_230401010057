44c932d624158dc16eb9da97007aeb85172d66cb
Ariel Gema Wardana <2024.arielgemaw.rplb@student.unsia.ac.id>
2025-05-04 00:58:44 +0700

    style(dashboard): add margin left

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
                 </ul>
             </aside>
 
-            <div class="p-6 bg-white text-medium text-gray-500 dark:text-gray-400 dark:bg-gray-800 rounded-lg w-full rounded-lg">
+            <div class="p-6 ml-2 bg-white text-medium text-gray-500 dark:text-gray-400 dark:bg-gray-800 rounded-lg w-full rounded-lg">
                 <?php
                 include 'dashboard.php';
                 ?>
