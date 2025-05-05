e7ff083cf00fcdcf6088f78dfbac51a8eda7e7a2
Ariel Gema Wardana <2024.arielgemaw.rplb@student.unsia.ac.id>
2025-05-05 14:35:16 +0700

    style(footer): adding copyright icon

--- layout/footer.php
+++ layout/footer.php
@@ -1,5 +1,5 @@
 <footer>
     <div class=" px-4 lg:px-12 py-3.5 text-right mx-auto max-w-screen-xl">
-        <p class="text-sm text-gray-500 dark:text-gray-400">@<?php echo date('Y'); ?> - <a href="">Ariel Gema Wardana</a>🚀</p>
+        <p class="text-sm text-gray-500 dark:text-gray-400">&copy; <?php echo date('Y'); ?> - <a href="">Ariel Gema Wardana</a>🚀</p>
     </div>
 </footer>
